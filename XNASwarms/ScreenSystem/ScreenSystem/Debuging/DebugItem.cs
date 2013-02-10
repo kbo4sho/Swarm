8e499b327397906d6e762c89b58fb5e944f2787a
Kevin.MacPro Bolander <kevinbolander@gmail.com>
2013-02-10 16:08:40 -0600

    debug file was being ignored

---
+++ XNASwarms/ScreenSystem/ScreenSystem/Debuging/DebugItem.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace ScreenSystem.Debug
+{
+    public enum DebugFlagType
+    {
+        
+        Normal = 0,
+        Odd = 1,
+        Important = 2,
+    }
+
+    public class DebugItem
+    {
+        private string Message;
+        private string Label;
+        private DebugFlagType DebugFlagType;
+
+        public DebugItem(string label, string message) :this(label,message,DebugFlagType.Normal)
+        {
+        }
+        
+        public DebugItem(string label, string message, DebugFlagType flagtype)
+        {
+            DebugFlagType = flagtype;
+            Message = message;
+            Label = label;
+
+        }
+
+        public string GetMessage()
+        {
+            return Message;
+        }
+
+        public string GetLabel()
+        {
+            return Label;
+        }
+
+        public DebugFlagType GetFlagType()
+        {
+            return DebugFlagType;
+        }
+
+
+        public string GetFormatedMessage()
+        {
+            return Label + ":   " + Message;
+        }
+
+        public Color GetColor()
+        {
+            switch (DebugFlagType)
+            {
+                case Debug.DebugFlagType.Normal:
+                    return Color.LightCyan;                    
+                case Debug.DebugFlagType.Odd:
+                    return Color.LightBlue;
+                case Debug.DebugFlagType.Important:
+                    return Color.LightCoral;
+            }
+            return Color.Blue;
+            
+        }
+    }
+}
---
+++ XNASwarms/ScreenSystem/ScreenSystem/Debuging/DebugScreen.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ScreenSystem.ScreenSystem;
+
+namespace ScreenSystem.Debug
+{
+
+    public interface IDebugScreen
+    {
+        void AddDebugItem(string label, string message, DebugFlagType flagtype);
+        void AddDebugItem(string label, string message);
+        void SetVisiblity();
+        void AddSpacer();
+    }
+
+
+    public class DebugScreen : DrawableGameComponent, IDebugScreen
+    {
+        private List<DebugItem> DebugItems;
+        private List<DebugItem> SavedDebugItems;
+
+        private Rectangle DebugPanelRectangle;
+        private Texture2D PanelTexture;
+        private ScreenManager screenManager;
+        FrameRateCounter frameratecounter;
+        private int itemSpacer;
+        private int PanelPadding;
+        private int MaxDebugItems;
+
+        private bool Visible;
+
+
+        public DebugScreen(ScreenManager screenmanager, bool visible)
+            : base(screenmanager.Game)
+        {
+            DebugItems = new List<DebugItem>();
+            SavedDebugItems = new List<DebugItem>(); 
+            screenManager = screenmanager;
+            Visible = visible;
+            
+        }
+
+        protected override void LoadContent()
+        {
+            //frameratecounter = new FrameRateCounter(screenManager);
+            //screenManager.Game.Components.Add(frameratecounter);
+            PanelTexture = screenManager.Content.Load<Texture2D>("Backgrounds/gray");
+            itemSpacer = 10;
+            PanelPadding = 10;
+            DebugPanelRectangle = new Rectangle(10, 10, 280, screenManager.GraphicsDevice.Viewport.Height - 50);
+            MaxDebugItems = (DebugPanelRectangle.Height - (PanelPadding * 2)) / itemSpacer;
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (Visible)
+            {
+                Vector2 largestStringSize = screenManager.Fonts.FrameRateCounterFont.MeasureString(DebugItems.OrderBy(s => s.GetFormatedMessage().Count()).Last().GetFormatedMessage().ToString());
+                //DebugPanelRectangle.Width 
+                DebugPanelRectangle.Width = (int)largestStringSize.X + PanelPadding * 3;
+                DebugPanelRectangle.Height = (itemSpacer * DebugItems.Count) + PanelPadding * 3;
+
+                if (DebugItems.Count > MaxDebugItems)
+                {
+                    DebugItems.RemoveRange(MaxDebugItems, DebugItems.Count - (MaxDebugItems));
+                }
+            }
+            base.Update(gameTime);
+        }
+
+        public override void Draw(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            if (Visible)
+            {
+                screenManager.SpriteBatch.Begin();
+                screenManager.SpriteBatch.Draw(PanelTexture, DebugPanelRectangle, new Color(20, 20, 20, 170));
+
+                for (int i = DebugItems.Count - 1; i >= 0; i -= 1)
+                {
+                    screenManager.SpriteBatch.DrawString(screenManager.Fonts.FrameRateCounterFont, DebugItems[i].GetFormatedMessage(),
+                                                          new Vector2(DebugPanelRectangle.X + PanelPadding, (DebugPanelRectangle.Y + itemSpacer * i) + PanelPadding), DebugItems[i].GetColor());
+                }
+                screenManager.SpriteBatch.End();
+
+            }
+            base.Draw(gameTime);
+        }
+
+        private void AddDebugItemSpacer()
+        {
+            DebugItems.Insert(0, new DebugItem("///////////////////////////////////////", "//////////////", DebugFlagType.Important));
+        }
+
+        #region IDebugScreen
+        public void AddDebugItem(string label, string message)
+        {
+            AddDebugItem(label, message, DebugFlagType.Normal);
+        }
+
+        public void AddDebugItem(string label, string message, DebugFlagType flagtype)
+        {
+            
+            switch (flagtype)
+            {
+                case DebugFlagType.Normal:
+                    DebugItems.Insert(0, new DebugItem(label, message, flagtype));
+                    break;
+                case DebugFlagType.Odd:
+                    DebugItems.Insert(0, new DebugItem(label, message,flagtype));
+                    //SavedDebugItems.Insert(0, new DebugItem(label, message, flagtype));
+                    break;
+                case DebugFlagType.Important:
+                    DebugItems.Insert(0, new DebugItem(label, message, flagtype));
+                    //SavedDebugItems.Insert(0, new DebugItem(label, message, flagtype));
+                    break;
+                    
+            }  
+        }
+
+        public void SetVisiblity()
+        {
+            Visible = !Visible;
+        }
+
+        public void AddSpacer()
+        {
+            AddDebugItemSpacer();
+        }
+
+        #endregion
+
+
+        
+
+       
+    }
+}
