40c638529851b50a566792590ca5e00bda240435
Jeff Meyers <jmeyers@colum.edu>
2013-06-11 16:30:31 -0500

    fixed git merge problem
    I love git

--- XNASwarms/SwarmEngine/WorldParameters.cs
+++ XNASwarms/SwarmEngine/WorldParameters.cs
@@ -11,18 +11,12 @@ namespace SwarmEngine
     /// </summary>
     public static class WorldParameters
     {
-<<<<<<< HEAD
-        //TODO Read values from config file
-        public static int numberOfIndividualsMax = 100;
-        public static int neighborhoodRadiusMax = 60;
-        public static int normalSpeedMax = 5;
-        public static int maxSpeedMax = 10;
-=======
+
         public static int numberOfIndividualsMax = 300;
         public static int neighborhoodRadiusMax = 45;
         public static int normalSpeedMax = 4;
         public static int maxSpeedMax = 8;
->>>>>>> 94bb674fcd2fcd1c367dd8e1bde36400522fa6c9
+
         public static double c1Max = 1;
         public static double c2Max = 1;
         public static double c3Max = 100;
